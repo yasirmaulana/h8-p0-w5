0a659884c614a4a5f259192236a817dcc2008d8b
yasirmaulana <yasir.maulana@gmail.com>
2018-01-26 18:26:39 +0700

    update tugas 2 week 5 dan menambahkan tugas 6 dan 7 week 5

--- exercise-2-change-me.js
+++ exercise-2-change-me.js
@@ -42,4 +42,4 @@ changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]);
 // 1. Christ Evans:{ firstName: 'Christ', lastName: 'Evans', gender: 'Male', age: 36 }
 // 2. Robert Downey:{ firstName: 'Robert', lastName: 'Downey', gender: 'Male', age: 'Invalid Birth Year' }
 
-// changeMe([]); // ""
+changeMe([]); // ""
